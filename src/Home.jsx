84293f2309767f24231ffb4e67cb50e227dc2377
reactapp-development <umanggg2006@gmail.com>
2026-07-14 11:32:27 +0530

    home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,12 @@
 const Home = () => {
     return (
-        <h1 className="text-dark">hello</h1>
+        <div>
+            <h1 className="text-dark">helli</h1>
+            <h1>this is home page</h1>
+            <h1>heloooooooooooooooooooooooo</h1>
+        </div>
+
+
     );
 }
  
